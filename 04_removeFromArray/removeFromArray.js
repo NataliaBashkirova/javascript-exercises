a9f2c5788c9e177b6baa639843ca7ae223a70cd9
NataliaBashkirova <bashkirova189@gmail.com>
2024-06-28 21:49:52 +0200

    Do exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...valuesToRemove) {
+  //
+  for (value of valuesToRemove) {
+    let index = inputArray.indexOf(value);
+    while (index !== -1) {
+        inputArray.splice(index, 1);
+        index = inputArray.indexOf(value);
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
