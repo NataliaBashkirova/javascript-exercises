c6a37a1bd79c05919c328458f5da09a9a7521a63
NataliaBashkirova <bashkirova189@gmail.com>
2024-07-01 00:31:30 +0200

    Do exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.toLowerCase().replaceAll(/[!., ]/g, "");
+  let middleOfTheWord = Math.floor(word.length/2);
+  for (let i = 0; i < middleOfTheWord; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
 };
 
+palindromes('ZZZZ car, a man, a maracaz.');
 // Do not edit below this line
 module.exports = palindromes;
