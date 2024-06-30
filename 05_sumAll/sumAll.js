6b7038af6e0ca88a6c3729fa1134b80fad40ff60
NataliaBashkirova <bashkirova189@gmail.com>
2024-06-30 23:18:42 +0200

    Do exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, finish) {
+  if (finish < start) {
+    let t = start;
+    start = finish;
+    finish = t;
+  }
 
+  if (
+    typeof start != "number" ||
+    typeof finish != "number" ||
+    start < 0 ||
+    finish < 0
+  ) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (let i = start; i <= finish; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
