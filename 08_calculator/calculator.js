bbd48aac8d72c9526348ffa108406a66f07189f9
NataliaBashkirova <bashkirova189@gmail.com>
2024-06-30 23:59:53 +0200

    Do exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,41 @@
-const add = function() {
-	
+const add = function(left, right) {
+  return left + right;	
 };
 
-const subtract = function() {
-	
+const subtract = function(left, right) {
+	return left - right;
 };
 
-const sum = function() {
-	
+const sum = function(operands) {
+	let sum = 0;
+  operands.forEach(x => sum += x);
+  return +sum;
 };
 
-const multiply = function() {
-
+const multiply = function(operands) {
+  let product = 1;
+  operands.forEach(x => product *= x);
+  return +product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let result = 1;
+  for (let i = 0; i < power; i++) {
+    result *= base;
+  }
+  return result;	
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  result = 1;
+  for (let i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return result;	
 };
 
+
+multiply([2, 4]);
 // Do not edit below this line
 module.exports = {
   add,
