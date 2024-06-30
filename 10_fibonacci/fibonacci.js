1d7dfacc1b37e88eae9160a3dd6fc9a2bf039802
NataliaBashkirova <bashkirova189@gmail.com>
2024-07-01 00:46:57 +0200

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+//   number = +number;
+  if (number == 0) return 0;
+  if (number < 0) return "OOPS";
+  if (number == 1 || number == 2) {
+    return 1;
+  } else {
+    previousNumber = 1;
+    prePreviousNumber = 1;
+    let currentNumber = 2;
+    for (let i = 2; i < number; i++) {
+      currentNumber = previousNumber + prePreviousNumber;
+      prePreviousNumber = previousNumber;
+      previousNumber = currentNumber;
+    }
+    return currentNumber;
+  }
 };
 
 // Do not edit below this line
