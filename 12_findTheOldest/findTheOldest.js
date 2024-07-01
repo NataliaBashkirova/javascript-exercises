94de86f6dabe4f8a01c9077a7035aa5739640626
NataliaBashkirova <bashkirova189@gmail.com>
2024-07-01 22:46:04 +0200

    Do exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+  return peopleArray.reduce(getOldest, peopleArray[0]);
 };
+
+function getOldest(oldest, current) {
+    if (getAge(current) > getAge(oldest)) {
+        return current;
+    } else {
+        return oldest;
+    }
+
+
+}
+
+function getAge(person) {
+    const CURRENT_YEAR = 2024;
+
+    if (!person.yearOfDeath) {
+        return CURRENT_YEAR - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
